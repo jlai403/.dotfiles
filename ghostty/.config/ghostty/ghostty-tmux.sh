d9810ac0454fc1cb0fbd5cda523678e0f4ba02c3
Joey <jlai@stellaralgo.com>
2025-12-04 16:44:03 -0700

    fix tmux ghostty config for ARM osx

--- ghostty/.config/ghostty/ghostty-tmux.sh
+++ ghostty/.config/ghostty/ghostty-tmux.sh
@@ -3,13 +3,14 @@ SESSION_NAME="ghostty"
 
 # Check if the session already exists
 # for some reason ghostty can't find tmux on launch
-/usr/local/bin/tmux has-session -t $SESSION_NAME 2>/dev/null
+tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? -eq 0 ]; then
- # If the session exists, reattach to it
- /usr/local/bin/tmux attach-session -t $SESSION_NAME
+  # If the session exists, reattach to it
+  tmux attach-session -t $SESSION_NAME
 else
- # If the session doesn't exist, start a new one
- /usr/local/bin/tmux new-session -s $SESSION_NAME -d
- /usr/local/bin/tmux attach-session -t $SESSION_NAME
+  # If the session doesn't exist, start a new one
+  tmux new-session -s $SESSION_NAME -d
+  tmux attach-session -t $SESSION_NAME
 fi
+
