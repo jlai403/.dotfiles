a7076249bad610ae6d4938ac939c55748afb3435
Joey <jlai@stellaralgo.com>
2025-12-18 11:49:03 -0700

    remote git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,20 +43,6 @@ function orphaned_symlinks() {
 }
 
 
-# git aliases
-alias gi='git init'
-alias gcl='git clone'
-alias gco='git checkout'
-alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
-alias ga='git add -A'
-alias gs='git status'
-alias gc='git commit'
-alias gp='git push'
-alias gu='git pull'
-alias gl="git log --all --graph --pretty=\
-	format:'%C(magenta)%h %C(white) %an %ar%C(auto) %D%n%s%n'"
-alias gb='git branch'
-
 # aws
 function av() {
 	aws-vault exec "$1" -- zsh -i
