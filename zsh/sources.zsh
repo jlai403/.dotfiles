8140222ff74f47f0bda252f1b22e7d700f59b6c1
joey <3282127+jlai403@users.noreply.github.com>
2026-04-10 14:37:03 -0600

    feat: source private zsh config at shell startup if present

--- zsh/sources.zsh
+++ zsh/sources.zsh
@@ -1,2 +1,5 @@
 source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Private/machine-local overrides (not tracked in public repo)
+[[ -f ~/.dotfiles_private/zsh/private.zsh ]] && source ~/.dotfiles_private/zsh/private.zsh
